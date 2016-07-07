5f9d391dea3344ca44bac6f531ce2dc597c29f06
Chiharu Shibata <chi-bd@live.jp>
2016-07-07 22:15:02 +0900

    not only export json, but also copy json to a clipboard at the same time.

--- src/js/components/publishing.js
+++ src/js/components/publishing.js
@@ -46,6 +46,7 @@ export default class Publishing extends React.Component {
     this.setState({
       json: JSON.stringify(this.props.texts)
     });
+    return this.state.json;
   }
 
   onSuccess() {
@@ -149,7 +150,7 @@ export default class Publishing extends React.Component {
         <div className="json-export">
           <div>
             <button onClick={this.handleImport.bind(this)}>import json</button>
-            <button onClick={this.handleExport.bind(this)}>export json</button>
+            <ClipboardButton option-text={this.handleExport.bind(this)} onSuccess={this.onSuccess}>export json</ClipboardButton>
           </div>
           <textarea value={this.state.json} onChange={this.handleInput.bind(this)} />
         </div>
